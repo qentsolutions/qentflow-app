1ae46720255ce6424e17d3ad2f99f8b866363e8f
qentsolutions <contact@qentsolutions.com>
2024-12-19 12:33:18 +0100

    fix description

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -12,6 +12,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { useCurrentWorkspace } from "@/hooks/use-current-workspace";
 import RichTextEditor from "./components/rich-text-editor";
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import Color from '@tiptap/extension-color';
+import TextStyle from '@tiptap/extension-text-style';
+import FontSize from 'tiptap-extension-font-size';
+
 interface DescriptionProps {
   data: CardWithList;
 }
@@ -55,12 +61,12 @@ export const Description = ({
     e.preventDefault();
     const boardId = params.boardId as string;
     const workspaceId = currentWorkspace?.id;
-    
+
     if (!workspaceId) {
       toast.error("Workspace ID is required.");
       return;
     }
-    
+
     execute({
       id: data.id,
       description,
@@ -68,10 +74,28 @@ export const Description = ({
       workspaceId,
     });
   }
+
+  const readOnlyEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Color.configure({ types: [TextStyle.name] }),
+      TextStyle,
+      FontSize.configure({
+        types: ['textStyle'],
+      }),
+    ],
+    content: description || 'Add a more detailed description...',
+    editable: false,
+    parseOptions: {
+      preserveWhitespace: true
+    }
+  });
+
+
   return (
     <div className="flex items-start gap-x-3 w-full">
       <div className="w-full">
-        <span className="flex items-center font-bold text-lg mb-6">
+        <span className="flex items-center font-bold text-lg mb-4">
           <TextAlignLeftIcon className="mr-2" /> Description
         </span>
         {isEditing ? (
@@ -102,11 +126,10 @@ export const Description = ({
             </div>
           </form>
         ) : (
-          <div
+          <EditorContent
             onClick={enableEditing}
-            role="button"
-            className="bg-gray-50 dark:bg-gray-700 min-h-[200px] text-gray-500 dark:text-gray-200 border text-sm py-3 px-3.5 rounded-md"
-            dangerouslySetInnerHTML={{ __html: description || "Add a more detailed description..." }}
+            editor={readOnlyEditor}
+            className="prose max-w-none p-4 bg-gray-50 rounded-lg border cursor-pointer"
           />
         )}
       </div>
