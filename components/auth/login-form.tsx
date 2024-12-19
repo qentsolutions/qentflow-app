127cb4a0dc8cf7bb2e74745fbd1fd4b1452d0d86
qentsolutions <contact@qentsolutions.com>
2024-12-19 11:18:35 +0100

    Revert "add"
    This reverts commit 49d2c1d4d9a0a88a389dad4249ad396950b4f87e.

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -70,6 +70,7 @@ export const LoginForm = () => {
 
   return (
     <CardWrapper
+      headerLabel="Welcome back"
       backButtonLabel="Don't have an account?"
       backButtonHref="/auth/register"
       showSocial
